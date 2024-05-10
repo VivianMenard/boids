48a7bec37f2486805e02ead2683bb67b18d4254a
vivian menard <menardvivian@gmail.com>
2024-05-10 18:01:11 +0200

    add docstrings in mathHelpers.cs (#18)

--- Assets/Scripts/MathHelpers.cs
+++ Assets/Scripts/MathHelpers.cs
@@ -1,20 +1,51 @@
 using UnityEngine;
 
+/// <summary>
+/// A collection of static helper methods for mathematical operations.
+/// </summary>
 public static class MathHelpers
 {
+    /// <summary>
+    /// A constant representing 1 / √2.
+    /// </summary>
     public const float OneOverSquareRootOfTwo = 0.7071067812f;
 
+    /// <summary>
+    /// Squares a number.
+    /// </summary>
+    /// 
+    /// <param name="number">The number to square.</param>
+    /// 
+    /// <returns>The squared number</returns>
     public static float Square(float number)
     {
         return number * number;
     }
 
+    /// <summary>
+    /// Computes cartesian coordinates of a point from its spherical coordinates (mathematics convention).
+    /// </summary>
+    /// 
+    /// <param name="center">The center of the spherical coordinates system.</param>
+    /// <param name="distance">
+    /// The radial distance in spherical coordinates (in R+) (distance between the point and the center).
+    /// </param>
+    /// <param name="theta">The azimuthal angle of the point in spherical coordinates (in [-π, π[).</param>
+    /// <param name="phi">The polar angle of the point in spherical coordinates (in [0, π]).</param>
+    /// <param name="thetaOffset">
+    /// [optional] Default 0, offset for theta in case the reference vector for theta angle isn't 
+    /// the x axis of the cartesian coordinate system.
+    /// </param>
+    /// 
+    /// <returns>
+    /// The cartesian coordinates of the point, in a coordinates system :
+    ///     - Whose center is the same than the one of the spherical coordinate system;
+    ///     - Whose x axis is the reference for the azimuthal angle (in thetaOffset = 0);
+    ///     - Whose y axis is the reference for the polar angle.
+    /// </returns>
     public static Vector3 SphericalToCartesian(
         Vector3 center, float distance, float theta, float phi, float thetaOffset = 0)
     {
-        // mathematics convention 
-        // distance in R+ (radial distance), theta in [-pi, pi[ (azimuthal angle), phi in [0, pi] (polar angle)
-
         Vector3 direction = new Vector3(
            Mathf.Sin(phi) * Mathf.Cos(thetaOffset + theta),
            Mathf.Cos(phi),
@@ -24,12 +55,29 @@ public static class MathHelpers
         return center + distance * direction;
     }
 
+    /// <summary>
+    /// Computes spherical coordinates of a point from its cartesian coordinates (mathematics convention).
+    /// </summary>
+    /// 
+    /// <param name="center">The center of the cartesian coordinates system.</param>
+    /// <param name="cartesianPosition">The position of the point in the cartesian coordiantes system.</param>
+    /// <param name="referenceForTheta">[Optional] Default <c>Vector2(1, 0)</c>, the reference to compute theta from (in the XZ plan).</param>
+
+    /// <returns>
+    /// The spherical coordinates of the point as a tuple (
+    ///     radial distance (in R+), 
+    ///     azimuthal angle theta in [-π, π[, 
+    ///     polar angle in [0, π],
+    /// ).
+    ///     
+    /// The spherical coordinates system is such that:
+    ///     - Its center is the same than the one of the cartesian coordinate system;
+    ///     - Its reference for the azimutal angle is <c>referenceForTheta</c> parameter;
+    ///     - Its reference for the polar angle is the y axis of the cartesian coordinate system.
+    /// </returns>
     public static (float, float, float) CartesianToSpherical(
         Vector3 center, Vector3 cartesianPosition, Vector2 referenceForTheta = default)
     {
-        // mathematics convention 
-        // distance in R+ (radial distance), theta in [-pi, pi[ (azimuthal angle), phi in [0, pi] (polar angle)
-
         if (referenceForTheta == default)
             referenceForTheta = Vector2.right;
 
@@ -49,11 +97,33 @@ public static class MathHelpers
         return (distance, theta, phi);
     }
 
+    /// <summary>
+    /// Determines whether a value is between two bounds (exclusively).
+    /// </summary>
+    /// 
+    /// <param name="value">The value to check.</param>
+    /// <param name="min">The minimum bound (exclusive).</param>
+    /// <param name="max">The maximum bound (exclusive).</param>
+    /// 
+    /// <returns>
+    /// <c>true</c> if the value is between the bounds (exclusively); otherwise, <c>false</c>.
+    /// </returns>
     public static bool IsBetween(float value, float min, float max)
     {
         return value > min && value < max;
     }
 
+    /// <summary>
+    /// Determines whether a point is inside a axis aligned box defined by its minimum and maximum points (exclusive).
+    /// </summary>
+    /// 
+    /// <param name="point">The point to check.</param>
+    /// <param name="minPt">The minimum point of the box (exclusive).</param>
+    /// <param name="maxPt">The maximum point of the box (exclusive).</param>
+    /// 
+    /// <returns>
+    /// <c>true</c> if the point is inside the box (exclusive); otherwise, <c>false</c>.
+    /// </returns>
     public static bool IsInBox(Vector3 point, Vector3 minPt, Vector3 maxPt)
     {
         return (
@@ -63,6 +133,17 @@ public static class MathHelpers
         );
     }
 
+    /// <summary>
+    /// Clamps each component of a vector within specified minimum and maximum values.
+    /// </summary>
+    /// 
+    /// <param name="initialVector">The initial vector to clamp.</param>
+    /// <param name="min">The minimum values for each component.</param>
+    /// <param name="max">The maximum values for each component.</param>
+    /// 
+    /// <returns>
+    /// A new vector with each component clamped within the specified range.
+    /// </returns>
     public static Vector3 ClampVector(Vector3 initialVector, Vector3 min, Vector3 max)
     {
         return new Vector3(
@@ -72,9 +153,20 @@ public static class MathHelpers
         );
     }
 
+    /// <summary>
+    /// Finds a point on the surface of an axis-aligned box, between two given points.
+    /// </summary>
+    /// 
+    /// <param name="ptIn">The point inside the box.</param>
+    /// <param name="ptOut">The point outside the box.</param>
+    /// <param name="minPt">The minimum point of the box.</param>
+    /// <param name="maxPt">The maximum point of the box.</param>
+    /// 
+    /// <returns>
+    /// The point on the surface of the box between the two given points.
+    /// </returns>
     public static Vector3 FindPointOnBoxBetween(Vector3 ptIn, Vector3 ptOut, Vector3 minPt, Vector3 maxPt)
     {
-        // Returns the point on the (axis aligned) box surface between a point in the box and another out of it. 
         float t = 0f;
 
         if (IsBetween(minPt.x, ptOut.x, ptIn.x))
@@ -93,6 +185,15 @@ public static class MathHelpers
         return Vector3.Lerp(ptOut, ptIn, t);
     }
 
+    /// <summary>
+    /// Generates a random direction vector in 3D space, optionally restricting the vertical angle.
+    /// </summary>
+    /// 
+    /// <param name="verticalRestriction">[Optional] Default 0, vertical restriction between 0 (no restrictions) and 1 (completely horizontal).</param>
+    /// 
+    /// <returns>
+    /// A random direction vector in 3D space.
+    /// </returns>
     public static Vector3 GetRandomDirection(float verticalRestriction = 0f)
     {
         float verticalRestrictionInRad = (Mathf.PI / 2) * Mathf.Clamp(
@@ -108,15 +209,31 @@ public static class MathHelpers
             Vector3.zero, 1f, theta, phi);
     }
 
+    /// <summary>
+    /// Converts a quaternion rotation to a forward direction vector.
+    /// </summary>
+    /// 
+    /// <param name="rotation">The quaternion rotation to convert.</param>
+    /// 
+    /// <returns>
+    /// The forward direction vector corresponding to the given rotation.
+    /// </returns>
     public static Vector3 RotationToDirection(Quaternion rotation)
     {
         return rotation * Vector3.forward;
     }
 
+    /// <summary>
+    /// Converts an angle to its equivalent within the range [-π, π[.
+    /// </summary>
+    /// 
+    /// <param name="initialAngle">The initial angle to convert in radians.</param>
+    /// 
+    /// <returns>
+    /// The equivalent angle within the range [-π, π[.
+    /// </returns>
     public static float EquivalentInTrigoRange(float initialAngle)
     {
-        // return the equivalent of the angle in [-pi, pi[ interval
-
         float moduloAngle = initialAngle % (2 * Mathf.PI);
         if (moduloAngle < 0)
             moduloAngle += (2 * Mathf.PI);
@@ -127,6 +244,16 @@ public static class MathHelpers
         return moduloAngle - 2 * Mathf.PI;
     }
 
+    /// <summary>
+    /// Restricts the vertical angle of a direction vector in 3D space.
+    /// </summary>
+    /// 
+    /// <param name="initialDirection">The initial direction vector to restrict.</param>
+    /// <param name="verticalRestriction">The vertical restriction to apply, between 0 (no restrictions) and 1 (completely horizontal).</param>
+    /// 
+    /// <returns>
+    /// The direction vector with the vertical angle restricted.
+    /// </returns>
     public static Vector3 RestrictDirectionVertically(Vector3 initialDirection, float verticalRestriction)
     {
         float verticalRestrictionInRad = (Mathf.PI / 2) * Mathf.Clamp(
